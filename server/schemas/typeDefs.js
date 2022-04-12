c40ebb42da1f669f260515b8fe86c182a2caac8c
jackarms <90922918+jackarms@users.noreply.github.com>
2022-04-12 18:05:50 -0500

    addition to typeDefs file

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -9,4 +9,23 @@ const typeDefs = gql`
     role: String
     membershipDate: Date
   }
+  type Pricing {
+    service: String
+    price: String
+  }
+  type Auth {
+    token: ID
+    user: User
+  }
+  type Query {
+      users: [User]
+      user:(email: String!): User
+      services: [Pricing]
+  }
+  type Mutation {
+    login(email: String!, password: String!): Auth
+    addUser(firstName: String!, lastName: String!, email: String!, password: String!): Auth
+  }
 `;
+
+module.exports = typeDefs;
