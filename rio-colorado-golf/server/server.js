ef00f96e98bf3d58c118b1ae4b1c178972d47aa1
veritascoding <jarrettcoding@gmail.com>
2022-04-11 20:20:39 -0500

    add basic express code to server.js

---
+++ rio-colorado-golf/server/server.js
@@ -0,0 +1,11 @@
+const express = require('express'); 
+const path = require('path'); 
+
+const db = require('./config/connection'); 
+
+const PORT = process.env.PORT || 3001; 
+const app = express(); 
+
+app.use(express.urlencoded({ extended: false })); 
+app.use(express.json()); 
+
