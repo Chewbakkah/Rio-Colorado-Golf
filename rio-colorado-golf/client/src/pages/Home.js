3d39d30adf489b3fe23febfca57bce12eb844e17
nickbanders <nickbanders@gmail.com>
2022-04-13 01:58:32 -0500

    some design changes on homepage

--- rio-colorado-golf/client/src/pages/Home.js
+++ rio-colorado-golf/client/src/pages/Home.js
@@ -1,6 +1,7 @@
 import React from "react";
 import crestLogo from "../../src/assets/img/riocolo-logo.png";
 import holeImage from "../../src/assets/img/riocolo-cup-slice.jpg";
+import holeImageGrad from "../../src/assets/img/riocolo-wht-gradient.png";
 import greenImage from "../../src/assets/img/riocolo-green.jpg";
 import Directions from "../components/Directions";
 import Footer from "../components/Footer";
@@ -15,7 +16,7 @@ const Home = () => {
             <img src={crestLogo} alt="Rio Colorado Logo" />
           </div>
         </section>
-
+        <img class="img-fluid w-100" src={holeImageGrad} />
         <img class="img-fluid w-100 img-shadow" src={holeImage} />
         <section class="intro-container">
           <div class="intro-welcome">
