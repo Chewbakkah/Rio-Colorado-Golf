a3c8ced6d271f49fd5620c4e1137cad13768e881
cdtest <corydavis1989@gmail.com>
2022-04-14 15:00:08 -0500

    updated auth.js

--- rio-colorado-golf/server/utils/auth.js
+++ rio-colorado-golf/server/utils/auth.js
@@ -1,15 +1,3 @@
-const jwt = require('jsonwebtoken');
-const secret = 'mysecretsshhhhh';
-const expiration = '2h';
-
-module.exports = {
-  authMiddleware: function ({ req }) {
-    // allows token to be sent via req.body, req.query, or headers
-    let token = req.body.token || req.query.token || req.headers.authorization;
-
-    // ["Bearer", "<tokenvalue>"]
-    if (req.headers.authorization) {
-      token = token.split(' ').pop().trim();
 const jwt = require("jsonwebtoken");
 
 const secret = "cheese";
@@ -26,31 +14,17 @@ module.exports = {
     }
 
     try {
-<<<<<<< HEAD
-      const { data } = jwt.verify(token, secret, { maxAge: expiration });
-      req.user = data;
-    } catch {
-      console.log('Invalid token');
-=======
       const { data } = jwt.verify(token, secret);
       req.user = data;
     } catch {
       console.log("Invalid Token");
->>>>>>> dev
     }
 
     return req;
   },
-<<<<<<< HEAD
-  signToken: function ({ firstName, email, _id }) {
-    const payload = { firstName, email, _id };
-
-    return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
-=======
 
   signToken: function ({ email, _id }) {
     const payload = { email, _id };
     return jwt.sign({ data: payload }, secret);
->>>>>>> dev
   },
 };
