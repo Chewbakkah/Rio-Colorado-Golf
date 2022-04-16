7b09c7efae72f72794c302c9856f3d2a288f85a8
nickbanders <nickbanders@gmail.com>
2022-04-16 02:42:21 -0500

    added animations to objects

---
+++ rio-colorado-golf/client/src/pages/Events.js
@@ -0,0 +1,33 @@
+import React from "react";
+import Calender from "../components/Calender/calender";
+import Placeholder from "../../src/assets/img/placeholder.png";
+
+const Events = () => {
+  return (
+    <section>
+      <div class="alligator-img">
+        <img class="img-fluid w-100 img-shadow" src={Placeholder} />
+      </div>
+      <div class="events-container">
+        <div class="missing-header">
+          <h2>EVENTS</h2>
+          <button class="calender-btn btn-shadow calender-button">
+            <a href="">VIEW CALENDER</a>
+          </button>
+        </div>
+
+        <Calender />
+      </div>
+      <div class="tournament-container">
+        <div class="missing-header">
+          <h2>TOURNAMENT RESULTS</h2>
+        </div>
+        <div class="tournament-results" id="tournament-results">
+          No Tournament Info Available
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Events;
