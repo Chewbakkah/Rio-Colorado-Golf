2134b22d1d9925f76f0da37a462d36cd7585bc20
veritascoding <jarrettcoding@gmail.com>
2022-04-16 16:59:54 -0500

    add apollo client code to app.js

--- rio-colorado-golf/client/src/App.js
+++ rio-colorado-golf/client/src/App.js
@@ -1,5 +1,13 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  createHttpLink,
+  createHttpLink,
+} from "@apollo/client";
+import { setContext } from "@apollo/client/link/context";
 
 import Nav from "./components/Nav";
 import Footer from "./components/Footer";
@@ -8,6 +16,25 @@ import Directions from "./components/Directions";
 import Home from "./pages/Home";
 import Amenities from "./pages/Amenities";
 import Course from "./pages/Course";
+
+const httpLink = createHttpLink({
+  uri: "/graphql",
+});
+
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem("id_token");
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : "",
+    },
+  };
+});
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
 
 function App() {
   return (
