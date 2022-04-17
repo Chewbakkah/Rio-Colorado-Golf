4585af59939b7d94d4d6171f8aca0acf7bec3fa0
nickbanders <nickbanders@gmail.com>
2022-04-16 19:02:55 -0500

    adding code for login and signup

--- rio-colorado-golf/client/src/App.js
+++ rio-colorado-golf/client/src/App.js
@@ -9,6 +9,8 @@ import Home from "./pages/Home";
 import Amenities from "./pages/Amenities";
 import Course from "./pages/Course";
 import Events from "./pages/Events";
+import Signup from "./pages/Signup";
+import Login from "./pages/Login";
 
 function App() {
   return (
@@ -21,6 +23,8 @@ function App() {
             <Route exact path="/course" component={Course} />
             <Route exact path="/amenities" component={Amenities} />
             <Route exact path="/events" component={Events} />
+            <Route exact path="/signup" component={Signup} />
+            <Route exact path="/login" component={Login} />
           </Switch>
         </div>
         <Footer />
