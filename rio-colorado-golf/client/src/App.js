6b509c502afc5e18d12e5864e49d2083f8f65645
Chewbakkah <chewbakkah@gmail.com>
2022-04-19 20:49:14 -0500

    production ready code

--- rio-colorado-golf/client/src/App.js
+++ rio-colorado-golf/client/src/App.js
@@ -21,7 +21,7 @@ import Contact from "./pages/Contact";
 import Admin from "./pages/Admin";
 
 const httpLink = createHttpLink({
-  uri: "http://localhost:3001/graphql",
+  uri: "/graphql",
 });
 
 // const authLink = setContext((_, { headers }) => {
